86a2b15f03b7158e66de6dd59521c1addefc8a16
luigidibella <lykos.omega@gmail.com>
2024-03-19 15:17:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp({
+  data(){
+    return{
+
+    }
+  },
+
+  methods: {
+    
+    
+  },
+
+  mounted() {
+    console.log('Hello Vue');
+    
+  },
+}).mount('#app')
