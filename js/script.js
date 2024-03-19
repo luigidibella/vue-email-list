1684a6bdf9248a0b5b5b0e8dbdb5b33e57003d7d
luigidibella <lykos.omega@gmail.com>
2024-03-19 16:41:38 +0100

    [add] milestone 1

--- js/script.js
+++ js/script.js
@@ -3,17 +3,28 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-
+      title: 'Vue Email List',
+      risultato: 'Loading...',
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emails: [],
     }
   },
 
   methods: {
-    
-    
+
+    getApis(){
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.apiUrl)
+        .then(res => {
+          this.emails.push(res.data.response)
+        })
+      }
+    },
+
   },
 
   mounted() {
-    console.log('Hello Vue');
+    this.getApis();
     
   },
 }).mount('#app')
